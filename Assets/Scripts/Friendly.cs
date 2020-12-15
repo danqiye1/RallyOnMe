e3d62f3921e305ad12006ab0095650e024f2e5ed
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2020-12-15 09:20:39 +0800

    Initial commit

---
+++ Assets/Scripts/Friendly.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friendly : MonoBehaviour
+{
+    public GameObject Officer;
+    public float FollowSpeed;
+    public float PanicDistance;
+
+    Animator anim;
+
+    int PanicHash = Animator.StringToHash("Panic");
+    int SpeedHash = Animator.StringToHash("Speed");
+    bool isFollowing = false;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hacky way to calculate Player's speed
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        Quaternion lookDir;
+
+        float PlayerDistance = Vector3.Distance(Officer.transform.position, transform.position);
+        if (PlayerDistance > PanicDistance && isFollowing == false)
+        {
+            // If player is not nearby, panic!
+            anim.SetBool(PanicHash, true);
+        }
+        else
+        {
+            // Start following player.
+            // The animatior controller will ensure the animation will no longer become panicked.
+            anim.SetBool(PanicHash, false);
+            isFollowing = true;
+
+            // Match follow speed with player
+            FollowSpeed = Officer.GetComponent<Player>().moveSpeed;
+            anim.SetFloat(SpeedHash, FollowSpeed * direction.magnitude);
+
+            // Move the player
+            Vector3 TargetPosition = Officer.transform.position + new Vector3(2,0,-2);
+            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, FollowSpeed * Time.deltaTime);
+            
+            // Rotation to look at where he is moving to.
+            if (transform.position != TargetPosition){
+                lookDir = Quaternion.LookRotation(TargetPosition - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookDir, Time.deltaTime);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    public float rotationSpeed = 100.0f;
+    public float rotationSmoothTime = 0.1f;
+    float rotationSmoothVelocity;
+
+    Animator anim;
+
+    public CharacterController controller;
+    public Transform cam;
+
+    int shootHash = Animator.StringToHash("Shoot");
+    int deathHash = Animator.StringToHash("Death");
+
+    // Shooting mechanics
+    public GameObject BulletPrefab;
+    public Vector3 BulletSpawnOffset; // Bullet spawn offset
+    public float BulletSpeed;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        // Walking
+        anim.SetFloat("Speed", direction.magnitude);
+
+        // Sprinting
+        if (Input.GetKey("left shift") && direction.magnitude >= 0.01f)
+        {
+            anim.SetBool("Sprint", true);
+            moveSpeed = 5.0f;
+        }
+        else
+        {
+            anim.SetBool("Sprint", false);
+            moveSpeed = 2.0f;
+        }
+
+        // Death
+        if (Input.GetKey("mouse 1"))
+        {
+            anim.SetInteger(deathHash, Random.Range(1, 4));
+        }
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // Player Rotation.
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSmoothVelocity, rotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            // Player movement
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+
+        // Shooting
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool(shootHash, true);
+            GameObject Bullet = Instantiate(BulletPrefab, transform.position + BulletSpawnOffset, Quaternion.Euler(90,0,0)); // Spawn a bullet with an offset with 0 rotation
+            Rigidbody rb = Bullet.GetComponent<Rigidbody>(); //Gets the rigidbody component from the bullet and stores it in rb
+            rb.AddForce(transform.forward * BulletSpeed, ForceMode.Impulse); //Move the bullet forward
+            // ShootSFXAudioSource.Play(); //Plays the shoot SFX
+            Destroy(Bullet, 1f); //Destroy bullet in 1 second.
+        }
+        else
+        {
+            anim.SetBool(shootHash, false);
+        }
+
+    }
+}
