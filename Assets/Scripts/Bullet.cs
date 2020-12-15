57adfb2543273b3632d3aac03bfd30e23b74f985
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2020-12-16 07:28:44 +0800

    Bullet collision

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject ImpactEffectPrefab;
+    GameObject ImpactEffect;
+    
+    // Destroy object and spawn effects when collide with something.
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.transform.CompareTag("Ground"))
+        {
+            ImpactEffect = Instantiate(ImpactEffectPrefab, transform.position, Quaternion.Euler(0, 0, 0));
+        }
+        Destroy(transform.gameObject);
+    }
+}
